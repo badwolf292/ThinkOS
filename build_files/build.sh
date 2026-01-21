2ff06e8c7e133b7e0365acac810efe77e5e2958b
badwolf292 <misc@ersdalolsen.no>
2026-01-21 19:48:01 +0100

    Remove unnessecary rclone package since it should already be included

--- build_files/build.sh
+++ build_files/build.sh
@@ -22,7 +22,7 @@ EOF
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y rclone nmap android-tools jotta-cli
+dnf5 install -y nmap android-tools jotta-cli
 
 # Use a COPR Example:
 #
