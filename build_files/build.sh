dde9a8032a7d6568077321f2a1f711cea8b2b700
badwolf292 <misc@ersdalolsen.no>
2026-03-26 15:49:15 +0100

    Remove nmap

--- build_files/build.sh
+++ build_files/build.sh
@@ -22,7 +22,7 @@ EOF
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y android-tools jotta-cli nmap
+dnf5 install -y android-tools jotta-cli
 
 # Use a COPR Example:
 #
